d65488667ab0a325dbcf5296e5f35b5e0252e7d7
Brandon Fields <brandonfields@Brandons-MacBook-Pro.local>
2020-03-24 12:31:45 -0400

    just working on some new things

---
+++ testrun/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HelloReact from "./components/HelloReact";
+import Navbar from "./components/Navbar";
+import Card from "./components/Section";
+
+function App() {
+  return (
+      <div>
+           <Navbar />
+           <HelloReact />
+           <Card />
+         
+      </div>
+  )
+  
+}
+
+export default App;
